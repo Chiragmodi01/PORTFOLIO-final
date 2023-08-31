dcd9749fbb5bf0dfb1b26c5d0b53283b36b89f55
Chiragmodi01 <chiragmodi2001@gmail.com>
2023-09-01 01:31:33 +0530

    refactor: form fields

--- script.js
+++ script.js
@@ -3,28 +3,24 @@ function setTheme(mode) {
     document.getElementById("theme-style").href = "default.css";
     document.getElementById("github").style.fill = "black";
     document.getElementById("mail").style.fill = "black";
-    document.getElementById("codepen").style.fill = "black";
   }
 
   if (mode === "blue") {
     document.getElementById("theme-style").href = "blue.css";
     document.getElementById("github").style.fill = "white";
     document.getElementById("mail").style.fill = "white";
-    document.getElementById("codepen").style.fill = "white";
   }
 
   if (mode === "green") {
     document.getElementById("theme-style").href = "green.css";
     document.getElementById("github").style.fill = "black";
     document.getElementById("mail").style.fill = "black";
-    document.getElementById("codepen").style.fill = "black";
   }
 
   if (mode === "purple") {
     document.getElementById("theme-style").href = "purple.css";
     document.getElementById("github").style.fill = "white";
     document.getElementById("mail").style.fill = "black";
-    document.getElementById("codepen").style.fill = "white";
   }
   localStorage.setItem("theme", mode);
 }
